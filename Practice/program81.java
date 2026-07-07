04e1a3c6db3b230c8da1f4c059ce813f4c95d5c5
Manasi Mali <manasimali06@gmail.com>
2026-07-07 18:51:18 +0530

    Add files via upload

---
+++ Practice/program81.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class program81
+{
+  public static void main(String A[])
+  {
+    Scanner sobj = new Scanner(System.in);
+    DigitX dobj = new DigitX();
+
+    int iValue = 0;
+
+    System.out.println("Enter number: ");
+    iValue = sobj.nextInt();
+
+    dobj.DisplayDigits(iValue);
+
+  }
+}
+
+class DigitX
+{
+  public void DisplayDigits(int iNo)
+  {
+
+    int iCnt = 0;
+    int iDigit = 0;
+
+    for(iCnt = 0; iCnt <= iNo; iCnt++)    // for( ; iNo != 0; )
+    {
+      iDigit = iNo % 10;
+      System.out.println(iDigit);
+
+      iNo = iNo / 10;
+      
+    }
+  }
+
+}
+
